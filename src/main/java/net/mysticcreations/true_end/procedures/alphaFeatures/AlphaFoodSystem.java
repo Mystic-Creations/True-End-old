f94f4f4557f65b463511d12d94ddef2d48926201
JustMili <rodx160@gmail.com>
2025-11-02 18:36:08 +0100

    1.4.2.7-wip-2

--- src/main/java/net/mysticcreations/true_end/procedures/alphaFeatures/AlphaFoodSystem.java
+++ src/main/java/net/mysticcreations/true_end/procedures/alphaFeatures/AlphaFoodSystem.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -41,7 +40,6 @@ public class AlphaFoodSystem {
 
     @SubscribeEvent
     public static int onRightClickItem(PlayerInteractEvent.RightClickItem event) {
-        if (event.getHand() != InteractionHand.MAIN_HAND) return 0;
         Player player = event.getEntity();
         if (player == null) return 0;
         if (player.level().dimension() != BTD) return 0;
@@ -50,16 +48,13 @@ public class AlphaFoodSystem {
         Float heal = FOOD_MAP.get(stack.getItem());
         float newHealth = player.getHealth();
 
-        int consumed;
+        int consumed = 2;
         if (heal != null) {
             newHealth += heal;
             consumed = 1;
         } else if (stack.isEdible()) {
             //Cancel if edible but not any of the listed items
             consumed = 0;
-        } else {
-            //Not edible but right-clickable? Skip cancelling
-            consumed = 2;
         }
 
         if (consumed == 1) {
@@ -75,17 +70,14 @@ public class AlphaFoodSystem {
             }
         }
 
-        if (consumed == 0) {
-            if (event.isCancelable()) {
-                event.setCanceled(true);
-            }
+        if (consumed == 0 && event.isCancelable()) {
+            event.setCanceled(true);
         }
         return consumed;
     }
 
     @SubscribeEvent
     public static int onRightClickItem(PlayerInteractEvent.RightClickBlock event) {
-        if (event.getHand() != InteractionHand.MAIN_HAND) return 0;
         Player player = event.getEntity();
         if (player == null) return 0;
         if (player.level().dimension() != BTD) return 0;
@@ -93,19 +85,15 @@ public class AlphaFoodSystem {
         ItemStack stack = event.getItemStack();
         Float heal = FOOD_MAP.get(stack.getItem());
 
-        int consumed;
+        int consumed = 2;
         if (heal != null) {
             consumed = 0;
         } else if (stack.isEdible()) {
             consumed = 0;
-        } else {
-            consumed = 2;
         }
 
-        if (consumed == 0) {
-            if (event.isCancelable()) {
-                event.setCanceled(true);
-            }
+        if (consumed == 0 && event.isCancelable()) {
+            event.setCanceled(true);
         }
         return consumed;
     }
@@ -113,16 +101,14 @@ public class AlphaFoodSystem {
     private static void playEatSound(LevelAccessor world, double x, double y, double z) {
         double pitch = 0.8 + Math.random() * 0.4;
 
-        if (world instanceof Level _level) {
-            if (!_level.isClientSide()) {
-                _level.playSound(null, BlockPos.containing(x, y, z),
-                    Objects.requireNonNull(
-                        ForgeRegistries.SOUND_EVENTS.getValue(ResourceLocation.parse("entity.generic.eat"))),
+        if (world instanceof Level level) {
+            if (!level.isClientSide()) {
+                level.playSound(null, BlockPos.containing(x, y, z),
+                    Objects.requireNonNull(ForgeRegistries.SOUND_EVENTS.getValue(ResourceLocation.parse("entity.generic.eat"))),
                     SoundSource.NEUTRAL, 1.0f, (float) pitch);
             } else {
-                _level.playLocalSound(x, y, z,
-                    Objects.requireNonNull(
-                        ForgeRegistries.SOUND_EVENTS.getValue(ResourceLocation.parse("entity.generic.eat"))),
+                level.playLocalSound(x, y, z,
+                    Objects.requireNonNull(ForgeRegistries.SOUND_EVENTS.getValue(ResourceLocation.parse("entity.generic.eat"))),
                     SoundSource.NEUTRAL, 1.0f, (float) pitch, false);
             }
         }
